0a1452bbf8e0895bc0e636e0ea8ae12eb3b4f16b
David <jorged.rubianog@utadeo.edu.co>
2020-06-29 21:22:50 -0500

    2do CRUD

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const methodOverride = require('method-override');
 const session = require('express-session');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const Handlebars = require('handlebars');
+const flash = require('connect-flash');
 
 // Initializations
 const app = express();
@@ -15,6 +18,7 @@ app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
     extname: '.hbs'
 }));
 app.set('view engine', '.hbs');
@@ -27,8 +31,15 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+app.use(flash());
 
 // Gobal Variables
+app.use((req, res, next) =>{
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+
+    next();
+});
 
 // Routes
 app.use(require('./routes/index'));
--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,8 +1,58 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/notes', (req, res) => {
-    res.send('Notes from databases');
+const Note = require('../models/Note') // metodos usar, put get, delete, etc
+
+router.get('/notes/add', (req, res) => {
+    res.render('notes/new-note')
+});
+
+router.post('/notes/new-note', async (req, res) => {
+    const {title, description} = req.body;
+    const errors = [];
+    if(!title){
+        errors.push({text:'Please Write a title'});
+        
+    }
+    if(!description){
+        errors.push({text: 'Please Write a description'});
+    }
+    if(errors.length > 0){
+        res.render('notes/new-note', {
+            errors,
+            title,
+            description
+        });
+    } else{
+        const newNote = new Note({title, description});
+        await newNote.save();
+        req.flash('success_msg', 'Note Added Succesfully')
+        res.redirect('/notes');
+    }
+    
+})
+
+router.get('/notes', async (req, res) => {
+    const notes = await Note.find().sort({date : 'desc'});
+    res.render('notes/all-notes', { notes });
+});
+
+router.get('/notes/edit/:id', async (req, res ) => {
+    const note =  await Note.findById(req.params.id);
+    res.render('notes/edit-notes', {note});
+});
+
+router.put('/notes/edit-notes/:id', async (req, res) =>{
+    const {title, description} = req.body;
+    await Note.findByIdAndUpdate(req.params.id, {title, description});
+    req.flash('success_msg', 'Note Updated Succesfully')
+    res.redirect('/notes')
+});
+
+router.delete('/notes/delete/:id', async (req, res) =>{
+    await Note.findByIdAndDelete(req.params.id);
+    req.flash('success_msg', 'Note Deleted Succesfully')
+    res.redirect('/notes');
 });
 
 module.exports  = router;

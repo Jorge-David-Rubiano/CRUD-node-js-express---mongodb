22ae08cbbd75e3114152f32377dd49cd4990b60a
David <jorged.rubianog@utadeo.edu.co>
2020-07-02 20:50:56 -0500

    5to Navigation

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -3,11 +3,13 @@ const router = express.Router();
 
 const Note = require('../models/Note') // metodos usar, put get, delete, etc
 
-router.get('/notes/add', (req, res) => {
+const {isAuthenticated} = require('../helpers/auth');
+
+router.get('/notes/add', isAuthenticated , (req, res) => {
     res.render('notes/new-note')
 });
 
-router.post('/notes/new-note', async (req, res) => {
+router.post('/notes/new-note', isAuthenticated , async (req, res) => {
     const {title, description} = req.body;
     const errors = [];
     if(!title){
@@ -32,27 +34,27 @@ router.post('/notes/new-note', async (req, res) => {
     
 })
 
-router.get('/notes', async (req, res) => {
+router.get('/notes', isAuthenticated , async (req, res) => {
     const notes = await Note.find().sort({date : 'desc'});
     res.render('notes/all-notes', { notes });
 });
 
-router.get('/notes/edit/:id', async (req, res ) => {
+router.get('/notes/edit/:id', isAuthenticated , async (req, res ) => {
     const note =  await Note.findById(req.params.id);
     res.render('notes/edit-notes', {note});
 });
 
-router.put('/notes/edit-notes/:id', async (req, res) =>{
+router.put('/notes/edit-notes/:id', isAuthenticated , async (req, res) =>{
     const {title, description} = req.body;
     await Note.findByIdAndUpdate(req.params.id, {title, description});
     req.flash('success_msg', 'Note Updated Succesfully')
     res.redirect('/notes')
 });
 
-router.delete('/notes/delete/:id', async (req, res) =>{
+router.delete('/notes/delete/:id', isAuthenticated , async (req, res) =>{
     await Note.findByIdAndDelete(req.params.id);
     req.flash('success_msg', 'Note Deleted Succesfully')
     res.redirect('/notes');
 });
 
-module.exports  = router;
+module.exports  = router; 
